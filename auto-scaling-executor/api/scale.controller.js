077025c7f620b4f6a779cf914f72ccb1a5c5d76e
Anjana Tinush <146495563+AnjanaTinush@users.noreply.github.com>
2025-11-19 21:59:40 +0530

    fixed

--- auto-scaling-executor/api/scale.controller.js
+++ auto-scaling-executor/api/scale.controller.js
@@ -1,26 +1,43 @@
 import express from "express";
-import LocalScaler from "../services/local-scaler.service.js";
+import K8sExecutor from "../services/k8s-executor.service.js";
 
 const router = express.Router();
 
-router.post("/scale", (req, res) => {
-  const { deployment, request_pods } = req.body;
+/**
+ * POST /api/v1/scale
+ */
+router.post("/scale", async (req, res) => {
+  try {
+    const { deployment, request_pods } = req.body;
 
-  if (!deployment || !request_pods) {
-    return res.status(400).json({
-      error: "deployment and request_pods are required",
+    if (!deployment || !request_pods) {
+      return res.status(400).json({
+        error: "deployment and request_pods fields are required",
+      });
+    }
+
+    // Calculation
+    const POD_CAPACITY = 50;
+    const requiredPods = Math.ceil(request_pods / POD_CAPACITY);
+
+    // CONNECT K8s Executor
+    const result = await K8sExecutor.performScaling(
+      deployment,
+      requiredPods
+    );
+
+    return res.json({
+      service: deployment,
+      previous_replicas: result.previous_replicas,
+      required_replicas: result.new_replicas,
+      scaled: result.scaling_applied,
+      verification: result.verify,
     });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Scaling failed" });
   }
-
-  const POD_CAPACITY = 50;
-  const requiredPods = Math.ceil(request_pods / POD_CAPACITY);
-
-  // LOCAL scaling simulation
-  const scalingResult = LocalScaler.simulateScaling(deployment, requiredPods);
-
-  return res.json({
-    ...scalingResult,
-  });
 });
 
 export default router;
